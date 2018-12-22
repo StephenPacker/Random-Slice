3c8c989411fa2e58e37c37e8c32078b6709df111
StephenPacker <spacker@ualberta.ca>
2018-12-21 22:13:03 -0700

    Improved readibility, testability and functionality of the generic porosity counter.

--- Shape Generalization/Generic_Porosity_Calc.py
+++ Shape Generalization/Generic_Porosity_Calc.py
@@ -1,38 +1,49 @@
 # This porosity calculator is designed to work with essentially any shape, assuming the scan only has one sample, and
-# that the sample is relatively closed (Picture pouring water on top of the provided images, if the water flows around
+# that the sample is relatively closed (Picture pouring syrup on top of the provided images, if the water flows around
 # the image that means the porosity calc should be accurate, if the water flows though the image the accuracy will
-# likely be much lower). In exchange for generality of shape, the computation time is much slower.
+# likely be much lower).
 
 import glob
 import cv2
 import numpy as np
+import time
+import matplotlib.pyplot as plt
+import random
+
+file_location = None
+loop_inc = 1  # Represents the step for the main loop, skips loop_inc - 1 images for faster runtime
+width_inc = 1  # Represents the inc/decrementer in shape outliner, bigger number skips more pixels for faster runtime
+save_count = 0  # Keeps track of how many comparison images are saved, solely for naming purposes
 
 
+# Main control for program, reads in images, iterates through images counting porosity via shape_outliner
 def main():
 
-	images = file_reader()
-
+	images = file_reader()  # Read in images
 	porosities = []
 
-	counter = 0
+	start = time.time()
 
-	for i in range(0, len(images), 500):
-		counter += 1
-		total_img_pixels = 0
-		total_shape_pixels = 0
-		shape = shape_outliner(images[i])
-		total_img_pixels += np.count_nonzero(images[i])
-		total_shape_pixels += np.count_nonzero(shape)
-		porosities.append(por_calc(total_img_pixels, total_shape_pixels))
+	# Iterate through the images and count the porosity
+	for i in range(0, len(images), loop_inc):
+		print(i)
+		total_img_pixels = np.count_nonzero(images[i])
+		if total_img_pixels != 0:
+			shape = shape_outliner(images[i])
+			total_shape_pixels = np.count_nonzero(shape)
+			porosities.append(por_calc(total_img_pixels, total_shape_pixels))
 
 	porosities = np.array(porosities)
+
+	end = time.time()
+	print("TIME: " + str(end - start))
 
 	print(np.average(porosities))
 
 
-# Absolutely brute force implementation that transposes the outline or shape of our data set onto a blank array that
-# can be referenced as an alternative to assuming all objects are circular in form. Essentially carves the general shape
-# Into a new 2D array! If run time is already slow, then fuck it!
+# Mimics a ROI shrink wrap procedure. Traces across original image until it encounters a white pixel, all the while
+# writing black pixels to the shape array. In doing this, it outlines the sample within each image,
+# which can then be used to calculate porosity.
 def shape_outliner(image):
 
 	height = image.shape[0]
@@ -40,27 +51,31 @@ def shape_outliner(image):
 	shape = np.ones((height, width), dtype=int)
 	cur_i = image
 
-	# Move left to right
+	# Move left to right across image moving down
 	for j in range(0, height):
 		width_index = width - 1
 		height_index = j
-		cur_p = cur_i[height_index, width_index]
-		while cur_p == 0 and width_index > 0:
-			width_index -= 1
+		cur_p = cur_i[height_index, width_index]  # Starting in top right corner
+		while cur_p == 0 and width_index > width_inc:  # Go until image has white pixel or reach boundary
+			width_index -= width_inc
 			cur_p = cur_i[height_index, width_index]
-			shape[height_index, width_index] = 0
+		shape[height_index, width_index:width] = 0
 
-	# Move right to left
+	# Move right to left moving down
 	for j in range(0, height):
 		width_index = 0
 		height_index = j
-		cur_p = cur_i[height_index, width_index]
-		while cur_p == 0 and width_index < width - 1:
-			width_index += 1
+		cur_p = cur_i[height_index, width_index]  # Starting in top left corner
+		while cur_p == 0 and width_index < width - width_inc:  # Go until image has white pixel or reach boundary
+			width_index += width_inc
 			cur_p = cur_i[height_index, width_index]
-			shape[height_index, width_index] = 0
+		shape[height_index, 0:width_index] = 0
 
-	shape = cv2.medianBlur(shape.astype(np.float32), 5)
+	shape = cv2.medianBlur(shape.astype(np.float32), 5)  # Sooth images to remove border irregularities
+
+	# Randomly choose to save a comparison image (Ensure shape outliner works properly)
+	if random_saver():
+		image_saver(image, shape)
 
 	return shape
 
@@ -71,6 +86,8 @@ def por_calc(bright_pixels, total_pixels):
 
 # Reads in image files specified by the users
 def file_reader():
+	global file_location
+
 	while True:
 		file_location = raw_input("Please specify the file path to where your images are stored: ")
 		file_type = raw_input("Please specify the file type. I.e bmp: ")
@@ -84,6 +101,28 @@ def file_reader():
 		break
 
 	return images
+
+
+# Saves a plot showing a image and its corresponding shape outline, allows user to ensure proper operation of code
+def image_saver(image, shape):
+	global save_count
+
+	plt.subplot(2, 1, 1)
+	plt.imshow(image, cmap="gray")
+	plt.subplot(2, 1, 2)
+	plt.imshow(shape, cmap="gray")
+	plt.savefig(file_location + " Comparison Number " + str(save_count) + ".jpg")
+	save_count += 1
+
+
+# Randomly saves an image based on a user determined probability "odds"
+def random_saver():
+	odds = 10  # Think of this as probability to save = 1/odds
+	x = random.randrange(odds)
+	if x == odds - 1:
+		return True
+	else:
+		return False
 
 
 main()
